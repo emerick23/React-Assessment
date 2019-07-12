d74ddaa499481b64627789033e789c4e934f40bb
Joe Thomas <Emerick23@yahoo.com>
2019-07-12 15:43:49 -0500

    Updates

---
+++ precache-manifest.8991d0f9dd78a83ddeac248496b0f089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac931139e5d9d62c77fb3fc6f579e230",
+    "url": "/emerick23/react-assessment/index.html"
+  },
+  {
+    "revision": "7e5d233afe20d87fd534",
+    "url": "/emerick23/react-assessment/static/css/main.feab412a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/emerick23/react-assessment/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7e5d233afe20d87fd534",
+    "url": "/emerick23/react-assessment/static/js/main.b7fa38e2.chunk.js"
+  },
+  {
+    "revision": "b6b3e8eef3ed165b516c",
+    "url": "/emerick23/react-assessment/static/js/runtime~main.91b6191a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-assessment/precache-manifest.8188fdf8a70e661cf553f0b2b763aa5f.js"
+  "/emerick23/react-assessment/precache-manifest.8991d0f9dd78a83ddeac248496b0f089.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-assessment/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/emerick23/react-assessment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
